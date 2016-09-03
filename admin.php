0bc504adac8b4efa7d1de53e8a60b0d905cf9599
Francois <fpoteau@gmail.com>
2016-09-03 15:32:26 +0200

    standardize letter case

--- admin.php
+++ admin.php
@@ -1,2 +1,2 @@
 <?php if(!defined('PLX_ROOT')) exit; ?>
-<?php header('location:parametres_plugin.php?p='.urlencode('plxcontentbackup')) ?>
+<?php header('location:parametres_plugin.php?p='.urlencode('plxContentBackup')) ?>
